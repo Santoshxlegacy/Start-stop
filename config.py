14e4129b7ba537ca7b96f412f1dfabeaaad11a50
Santoshxlegacy <santoshxlegacy@outlook.com>
2024-12-24 04:07:56 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY 
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7300928635:AAG8uKhoY4rVhe6tyOIXuOrU6Yl6rCYKhFM")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("EVIL4REAL0")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1342302666")
+
+
+
+
+
+
+
+  
